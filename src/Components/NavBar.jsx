98ee2185afc070266dc90990d4d53c4750bd8ad0
Kaic Machado <kaicmachado2@gmail.com>
2024-11-19 16:42:12 -0300

    adicionado href ao button da navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -17,7 +17,8 @@ const NavBar = () => {
      </li>
     </ul>
     <a
-     href=""
+     target="_blank"
+     href="https://api.whatsapp.com/send?phone=+5516992967026&text=Ol%C3%A1%2C+gostaria+de+fazer+um+orcamento"
      className="p-3 border rounded-xl gap-4 hidden sm:flex font-light"
     >
      <img src={LogoWhats} width={25} alt="Logo do Whatsapp" />
